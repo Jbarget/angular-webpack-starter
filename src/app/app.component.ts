62ae935439fba6322d6f13d138d68a1ee8ae211d
Justen <justenbarget@gmail.com>
2016-11-27 18:42:33 +0000

    removing commented code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,3 @@ import '../../public/css/styles.css';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent { }
-
-// import {Component} from 'angular2/core';
-// import NavBar from './nav.component';
-// import Chart from './chart.component';
-//
-// @Component({
-//   selector: 'main-app',
-//   template: `
-//     <div class="main-app">
-//       <nav-bar></nav-bar>
-//       <chart></chart>
-//     </div>`,
-//   directives: [NavBar]
-// })
-//
-// export class AppComponent {
-//   constructor() {}
-// }
